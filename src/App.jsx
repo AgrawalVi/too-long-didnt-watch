6ec9169989f3de4e0cee1e02cb49b76bcc740bf7
Vishrut <vagrawal24@wisc.edu>
2024-02-26 22:17:08 -0600

    Initial Commit Setup React with Vite and Tailwind for CSS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Welcome to TLDR!</h1><object data="" type=""></object>
+      </div>
+    </>
+  )
+}
+
+export default App
